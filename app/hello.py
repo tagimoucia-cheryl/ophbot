008c4f245222995d21fe3e33ee5ad875a994fcac
Steve Harris <git@steveharris.me>
2021-03-03 16:47:33 +0000

    re-arranged to separate package installs and app

--- app/hello.py
+++
@@ -1,3 +0,0 @@
-import datetime
-now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-print('current timestamp: ' + now)
---
+++ src/packages/dockR/R/utils.R
@@ -0,0 +1,4 @@
+#' Simple hello world function used for debugging
+say_hello <- function() {
+    print('Hello from Rocker + tidyverse + emapR')
+}
